03317b1215ae08925dce81426c7fcf01749ba5c3
zhanggang <zhanggang@outlook.com>
2020-01-30 19:29:35 +0800

    blog滚动条处理

---
+++ src/main/java/com/zhanggang/blog/handler/ControllerExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.zhanggang.blog.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+}
--- src/main/java/com/zhanggang/blog/web/indexController.java
+++ src/main/java/com/zhanggang/blog/web/indexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class indexController {
     @GetMapping("blog")
     public String blog(){
+        int a = 9/0;
         return "blog";
     }
 }

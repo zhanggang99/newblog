594da074c339727d46b71a7ee48a517ca2b12c2f
zhanggang <zhanggang@outlook.com>
2020-01-31 14:57:13 +0800

    fragment模板处理

--- src/main/java/com/zhanggang/blog/aspect/LogAspect.java
+++ src/main/java/com/zhanggang/blog/aspect/LogAspect.java
@@ -1,10 +1,16 @@
 package com.zhanggang.blog.aspect;
 
+import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.spi.CalendarDataProvider;
 
 @Aspect
@@ -17,8 +23,16 @@ public class LogAspect {
 
     }
     @Before("log()")
-    public void doBefore(){
-        logger.info("-------before-------");
+    public void doBefore(JoinPoint joinPoint){
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String url = request.getRequestURI();
+        String ip = request.getRemoteAddr();
+        String classMethod = joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        RequestLog requestLog = new RequestLog(url,ip,classMethod,args);
+//        logger.info("-------before-------");
+        logger.info("Request {}",requestLog.toString());
     }
     @After("log()")
     public void doAfter(){
@@ -26,6 +40,29 @@ public class LogAspect {
     }
     @AfterReturning(returning = "result",pointcut = "log()")
     public void doAfterReturn(Object result){
-        logger.info("result :{}",result);
+        logger.info("Result :{}",result);
+    }
+    private class RequestLog{
+        private String url;
+        private String ip;
+        private String classMethod;
+        private Object[] args;
+
+        public RequestLog(String url, String ip, String classMethod, Object[] args) {
+            this.url = url;
+            this.ip = ip;
+            this.classMethod = classMethod;
+            this.args = args;
+        }
+
+        @Override
+        public String toString() {
+            return "RequestLog{" +
+                    "url='" + url + '\'' +
+                    ", ip='" + ip + '\'' +
+                    ", classMethod='" + classMethod + '\'' +
+                    ", args=" + Arrays.toString(args) +
+                    '}';
+        }
     }
 }

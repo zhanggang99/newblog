3ad9e04ff4ed32cecc7cae082436aa0230cc9cde
zhanggang <zhanggang@outlook.com>
2020-02-07 23:23:12 +0800

    博客页面

--- src/main/java/com/zhanggang/blog/web/indexController.java
+++ src/main/java/com/zhanggang/blog/web/indexController.java
@@ -12,6 +12,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -38,5 +39,10 @@ public class indexController {
         model.addAttribute("page",blogService.listBlog("%"+query+"%",pageable));
         model.addAttribute("query",query);
         return "search";
+    }
+    @GetMapping("/blog/{id}")
+    public String blog(@PathVariable Long id,Model model){
+        model.addAttribute("blog",blogService.getBlog(id));
+        return "blog";
     }
 }

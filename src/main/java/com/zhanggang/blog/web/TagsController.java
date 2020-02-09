005f86f3bd153edbce5a6c785ef2b47b94f9181d
zhanggang <zhanggang@outlook.com>
2020-02-09 20:12:40 +0800

    页面整理

--- src/main/java/com/zhanggang/blog/web/TagsController.java
+++ src/main/java/com/zhanggang/blog/web/TagsController.java
@@ -2,12 +2,13 @@ package com.zhanggang.blog.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class TagsController {
 
-    @GetMapping("/tags")
-    public String tags(){
+    @GetMapping("/tags/{id}")
+    public String tags(@PathVariable Long id){
 
         return "/tags";
     }
--- src/main/java/com/zhanggang/blog/web/TypesController.java
+++ src/main/java/com/zhanggang/blog/web/TypesController.java
@@ -1,15 +1,46 @@
 package com.zhanggang.blog.web;
 
+import com.zhanggang.blog.po.Type;
+import com.zhanggang.blog.service.BlogService;
+import com.zhanggang.blog.service.TypeService;
+import com.zhanggang.blog.vo.BlogQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class TypesController {
 
-    @GetMapping("/types")
-    public String types(){
+    @Autowired
+    TypeService typeService;
+    @Autowired
+    BlogService blogService;
+//
+//    @GetMapping("/types")
+//    public String types(Model model){
+//        model.addAttribute("types",typeService.listTypeTop(10000));
+//        return "types";
+//    }
 
-        return "/types";
+    @GetMapping("/types/{id}")
+    public String types(@PageableDefault(size = 10,sort = {"updateTime"},direction = Sort.Direction.DESC) Pageable pageable, @PathVariable Long id, Model model){
+        List<Type> types = typeService.listTypeTop(10000);
+        if (id == -1){
+            id=types.get(0).getId();
+        }
+        BlogQuery query = new BlogQuery();
+        query.setTypeId(id);
+        model.addAttribute("types",types);
+        model.addAttribute("page",blogService.listBlog(pageable,query));
+        model.addAttribute("activeTypeId",id);
+        return "types";
     }
 }
